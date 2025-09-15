4d1c99c942aa594457776f250f62fe24b2f3ee3a
Ellyj12 <ellyjjemba@gmail.com>
2025-09-15 17:56:04 +0300

    Finished week 7 Assignment

---
+++ script.js
@@ -0,0 +1,27 @@
+// ------------------ Constants ------------------
+// Grab DOM elements that will be interacted with
+const ScrollBtn = document.getElementById('scroll')
+const CardSection = document.getElementById('CardSection')
+const LoginForm = document.getElementById('LoginForm')
+const LoginBtn = document.getElementById('LogInBtn')
+const CloseBtn = document.getElementById('CloseBtn')
+
+// ------------------ Functions ------------------
+// Toggle visibility of an element by adding/removing the 'shown' class
+function toggleElementVisibility(element) {
+    if (!element) return false;  // Check if the element has been defined
+    element.classList.toggle('shown');
+    return true;
+}
+
+// ------------------ Event Listeners ------------------
+// Show login modal when login button is clicked
+LoginBtn.addEventListener('click', () => toggleElementVisibility(LoginForm))
+
+// Hide login modal when close button is clicked
+CloseBtn.addEventListener('click', () => toggleElementVisibility(LoginForm))
+
+// Scroll section2 into view smoothly when scroll button 
+ScrollBtn.addEventListener('click', function () {
+    CardSection.scrollIntoView({ behavior: 'smooth' })
+})
